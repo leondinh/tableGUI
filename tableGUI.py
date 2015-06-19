0616e5535b502f95527788f0863f50c1adf83e64
Leon Dinh <ldinh@wisc.edu>
2015-06-18 21:10:15 -0500

    Added menu bar, sorting, masking.

--- tableGUI.py
+++ tableGUI.py
@@ -1,14 +1,17 @@
 import sys
 from pandas import *
 from PyQt4.QtGui import *
+import PyQt4.QtCore as QtCore
 
-def __baseView(dataframe, editCellOnly_flag, addRows_flag):
+df = read_csv("test.csv")
+def __baseView(dataframe, editOnly_list=None, addRows_flag=False, nonEditableEnabled_flag=True):
 	# Create an PyQT4 application object.
 	app 		= QApplication(sys.argv)
 	window 		= QMainWindow()
 	gridLayout 	= QGridLayout()
 	centralWidget 	= QWidget()
-
+        menuBar         = QMenuBar()
+        
 	# Create window w/ table
 	datatable = QTableWidget()
 
@@ -24,10 +27,6 @@ def __baseView(dataframe, editCellOnly_flag, addRows_flag):
 		addRowButton.clicked.connect(handleAddRowButton)
 		addRowButton.show()
 
-	# Disable edit
-	if not editCellOnly_flag:
-		datatable.setEditTriggers(QAbstractItemView.NoEditTriggers)
-
 	# Set number of columns and rows
 	datatable.setColumnCount(len(dataframe.columns))
 	datatable.setRowCount(len(dataframe.index))
@@ -40,17 +39,40 @@ def __baseView(dataframe, editCellOnly_flag, addRows_flag):
 	for i in range(len(dataframe.index)):
 		for j in range(len(dataframe.columns)):
 			datatable.setItem(i,j,QTableWidgetItem(str(dataframe.iget_value(i, j))))
-	gridLayout.addWidget(datatable,0,0)
+	gridLayout.addWidget(datatable,1,0)
+	
 	if addRows_flag:
 		gridLayout.addWidget(addRowButton,0,1)
 
+	# Add menu bar
+        gridLayout.addWidget(menuBar,0,0)
+        fileMenu = menuBar.addMenu('File')
+        exitAction = QAction('Exit', window)
+        exitAction.triggered.connect(exit)
+        fileMenu.addAction(exitAction)
+        
 	window.setCentralWidget(centralWidget)
 	centralWidget.setLayout(gridLayout)
 
+        if editOnly_list is not None:
+                for i in range(datatable.columnCount()):
+                        headerName = datatable.horizontalHeaderItem(i).text()
+                        inEditList = False
+                        for j in range(len(editOnly_list)):
+                                if headerName == editOnly_list[j]:
+                                        inEditList = True
+                        if inEditList == False:
+                                for k in range(datatable.rowCount()):
+                                        if nonEditableEnabled_flag == True:
+                                                datatable.item(k, i).setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+                                        else:
+                                                datatable.item(k, i).setFlags(QtCore.Qt.ItemIsSelectable)
+
+        # Enable sorting on columns
+        datatable.setSortingEnabled(True)
+        
 	# Set window size
-	width = min((j+1)*105, app.desktop().screenGeometry().width() - 50)
-	height = min((i+1)*41, app.desktop().screenGeometry().height() - 100)
-	window.resize(width, height)
+	window.resize(430, 250)
  
 	# Set window title  
 	window.setWindowTitle("") 
@@ -62,11 +84,11 @@ def __baseView(dataframe, editCellOnly_flag, addRows_flag):
 	return datatable
 
 def view(dataframe):
-	__baseView(dataframe, False, False)
+	__baseView(dataframe, [], nonEditableEnabled_flag=True)
 	return
 
-def edit(dataframe, addRows_flag):
-	datatable = __baseView(dataframe, True, addRows_flag)
+def edit(dataframe, editOnly_list=None, addRows_flag=False):
+	datatable = __baseView(dataframe, editOnly_list, addRows_flag, nonEditableEnabled_flag=False)
 
 	# Create new dataframe to return
 	index = []
@@ -80,5 +102,5 @@ def edit(dataframe, addRows_flag):
 			if not datatable.item(i,j):
 				dfNew.set_value(dfNew.index[i], dfNew.columns[j], "")
 			else:
-				dfNew.set_value(dfNew.index[i], dfNew.columns[j], str(datatable.item(i, j).text()))	
+				dfNew.set_value(dfNew.index[i], dfNew.columns[j], str(datatable.item(i, j).text()))
 	return dfNew
